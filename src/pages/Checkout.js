afe4515acc7ce56f1c25183a43ce0f56b57cb391
topik96 <topikmujianto@topiks-MacBook-Air.local>
2018-04-24 04:06:05 +0700

    screen checkout

--- src/pages/Checkout.js
+++ src/pages/Checkout.js
@@ -1,79 +1,88 @@
 import React from 'react'
-import { View, Alert,AsyncStorage } from 'react-native'
+import { View, Alert, AsyncStorage } from 'react-native'
 import {
     ListItem,
     Body,
     List,
     Content,
-    Text, 
-    Button, 
-    Right, 
-    Left, 
-    Card, 
-    CardItem, 
-    Container, 
-    Header, 
-    Icon, 
-    Title, 
-    Item, 
+    Text,
+    Button,
+    Right,
+    Left,
+    Card,
+    CardItem,
+    Container,
+    Header,
+    Icon,
+    Title,
+    Item,
     Input
 } from 'native-base'
 import axios from 'axios'
 import Payment from './Payment'
 export default class Checkout extends React.Component {
-    constructor(){
+    constructor() {
         super()
         this.state = {
-            username:'', address:'',phone:'',paymentOrder:0,idOrder:'',email:'',name:'',redirectURL:''
+            username: '', 
+            address: '', 
+            phone: '', 
+            paymentOrder: 0, 
+            idOrder: '', 
+            email: '', 
+            name: '', 
+            redirectURL: '', 
+            userID:0,
+            tempCart:[]
         }
     }
-    componentWillMount() {
+    componentDidMount() {
         AsyncStorage.getItem('idUser')
-        .then(res=>{
-            axios.get(`http://192.168.100.29:5000/api/users/${res}`)
-            .then(r=>{
-                this.setState({
-                    username:r.data[0].username,
-                    phone:r.data[0].phone,
-                    address:r.data[0].address
-                })
-                console.log(r.data[0].phone +' address '+r.data[0].address)
+            .then(res => {
+                axios.get(`http://192.168.100.10:3000/api/users/?id=${res}`)
+                    .then(r => {
+                        this.setState({
+                            userID: r.data[0].id,
+                            username: r.data[0].username,
+                            phone: r.data[0].phone,
+                            address: r.data[0].address
+                        })
+                        console.log(r.data[0].phone + ' address ' + r.data[0].address)
+                    })
             })
-        })
+
+        AsyncStorage.getItem('itemInCart')
+            .then(res=>{
+                this.setState({
+                    tempCart:JSON.parse(res)
+                })
+                
+            })
 
         AsyncStorage.getItem('checkout')
-        .then(res=>{
-            let data=JSON.parse(res)
-            this.setState({
-                paymentOrder:data.total,
-                idOrder:data.id
+            .then(res => {
+                let data = res
+                this.setState({
+                    paymentOrder: data,
+                })
+                console.log(data + ' Data order ')
             })
-            console.log(data.total+' Data order ')
-        })
     }
 
-    requestRedirectUrl(){
-        let data = JSON.stringify({
-            "transaction_details": {
-              "order_id": this.state.idOrder,
-              "gross_amount": this.state.paymentOrder
-            }
-          })
-    
-    let axiosConfig = {
-        headers:{
-           'Accept':'application/json',
-           'Content-Type':'application/json',
-           'Authorization':`Basic U0ItTWlkLXNlcnZlci13OFN3cFlmZUN6Umt4QXE4cFJTajNUWHM6`
+    requestRedirectUrl() {
+        let data = {
+            "users_id": parseInt(this.state.userID),
+            "data": this.state.tempCart,
+            "total": this.state.paymentOrder
         }
-    }
-    axios.post(`https://app.sandbox.midtrans.com/snap/v1/transactions`,data, axiosConfig)
-    .then(res=>{
-        this.props.navigation.navigate('Payment',res.data.redirect_url)
-    })
-    .catch(err=>{
-        console.log(err +' gan')
-    })
+
+        axios.post(`http://192.168.100.10:3000/api/orders`, data)
+            .then(res => {
+                this.props.navigation.navigate('Payment',res.data.redirect_url)
+            })
+            .catch(err => {
+                console.log(err + ' gan')
+            })
     }
 
     render() {
@@ -86,13 +95,15 @@ export default class Checkout extends React.Component {
                                 'Notice',
                                 'Cancel Payment',
                                 [
-                                  {text: 'Cancel', onPress: () => console.log('Cancel Pressed'), style: 'cancel'},
-                                  {text: 'OK', onPress: () => {
-                                      this.props.navigation.navigate('TabView')
-                                    }},
+                                    { text: 'Cancel', onPress: () => console.log('Cancel Pressed'), style: 'cancel' },
+                                    {
+                                        text: 'OK', onPress: () => {
+                                            this.props.navigation.navigate('TabView')
+                                        }
+                                    },
                                 ],
                                 { cancelable: false }
-                              )
+                            )
                         }}>
                             <Icon name="arrow-back" />
                         </Button>
@@ -110,19 +121,11 @@ export default class Checkout extends React.Component {
                         <CardItem bordered>
                             <Body>
                                 <Text>Please make your payment of <Text style={{ fontWeight: 'bold' }}>${this.state.paymentOrder}</Text></Text>
-                                <Item>
-                                    <Input placeholder="Received Name" onChangeText={(txt)=>{
-                                        this.setState({
-                                            name:txt
-                                        })
-                                    }}/>
-                                    </Item>
                             </Body>
                         </CardItem>
                         <CardItem footer>
                             <Button full primary onPress={() => {
                                 this.requestRedirectUrl()
-                                
                             }}>
                                 <Text>                           Confirm                             </Text>
                             </Button>
